f8aa318a7f0c9fb6fc6c3abe3fc5039518a4ca35
RalphBacon <20911308+RalphBacon@users.noreply.github.com>
2021-12-08 13:30:50 +0000

    Refresh 1v36
    Version updated to reflect enhanced error handling for poor Internet connectivity.

--- Sketch/ESP32 Better Internet Radio - YouTube/include/version.h
+++ Sketch/ESP32 Better Internet Radio - YouTube/include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "35"
+  #define BUILD_NUMBER "36"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.210701_35 - 2021-07-01 14:37:42.854906"
+  #define VERSION "v1.210701_36 - 2021-12-08 13:10:12.436186"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.35"
+  #define VERSION_SHORT "v1.36"
 #endif
